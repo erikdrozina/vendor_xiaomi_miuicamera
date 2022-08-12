0647a47215dbbf9a7f762e8713a7e1d53038e2a2
Erik Drozina <erikdro41@gmail.com>
2022-08-13 01:07:43 +0200

    miuicamera: Import misys stuff
    Co-authored-by: neobuddy89 <neobuddy89@gmail.com>
    Co-authored-by: Demon000 <demonsingur@gmail.com>

--- BoardConfigAnx.mk
+++ BoardConfigAnx.mk
@@ -1,5 +1,9 @@
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/xiaomi/miuicamera/configs/hidl/framework_compatibility_matrix.xml
+
 # Sepolicy
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    vendor/xiaomi/miuicamera/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += vendor/xiaomi/miuicamera/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += vendor/xiaomi/miuicamera/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += vendor/xiaomi/miuicamera/sepolicy/vendor
 
 SELINUX_IGNORE_NEVERALLOWS := true
--- config.mk
+++ config.mk
@@ -6,11 +6,20 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib,$(TARGET_COPY_OUT_SYSTEM)/lib) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/priv-app/ANXCamera/lib/arm64,$(TARGET_COPY_OUT_SYSTEM)/priv-app/ANXCamera/lib/arm64) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/bin/hw,$(TARGET_COPY_OUT_VENDOR)/bin/hw) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/etc/init,$(TARGET_COPY_OUT_VENDOR)/etc/init) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/lib,$(TARGET_COPY_OUT_VENDOR)/lib) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64) \
 
 PRODUCT_PACKAGES += \
-    ANXCamera
+    ANXCamera \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0.vendor \
+    android.hidl.memory.block@1.0 \
+    android.hidl.memory.block@1.0.vendor \
+    vendor.xiaomi.hardware.misys@1.0 \
+    vendor.xiaomi.hardware.misys@2.0 \
+    vendor.xiaomi.hardware.misys@3.0
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.miui.notch=1
